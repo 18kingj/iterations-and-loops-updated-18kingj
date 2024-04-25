0a77dd54fca19d547cf9fc577fe89989046e1a9b
18kingj <161600931+18kingj@users.noreply.github.com>
2024-04-25 08:13:45 +0000

    Update

--- exercises/exercise_25.py
+++ exercises/exercise_25.py
@@ -1,10 +1,10 @@
 # Your solution to Exercise 25
 d= int(input())
 t = int(input())
-
+dc = d
 count = 1
 while d < t:
-    d *= 1.1
+    d = d + (dc*1.1)
     count += 1
 
 print(f"{d:.2f} km, {count} days")
--- exercises/exercise_39.py
+++ exercises/exercise_39.py
@@ -1,11 +1,11 @@
 # Your solution to Exercise 39
 n = int(input())
-sum = 0
-n += -1 if n < 0 else 1
+total = 0
+n *= -1 if n < 0 else 1
 while n != 0:
     d = n % 10
-    sum += d
+    total += d
     n //= 10
-if sum == 37:
-    sum = 45
-print(sum)
+# if total == 37:
+#     total = 45
+print(total)
